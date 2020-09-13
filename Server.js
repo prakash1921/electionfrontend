b8b655c34e8a7f779b58360cc5046359c9ea9e15
prakash1921 <dhobi.prakash1920@gmail.com>
2020-09-13 19:16:52 +0530

    changes done

--- Server.js
+++ Server.js
@@ -3,12 +3,12 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 // var port = process.env.PORT;
-app.get('*', function (req, res) {
-    res.sendFile('index.html');
-  });
-// app.get('/', function(req, res) {
-//     console.log('__dirname',__dirname)
-//     res.sendFile(path.join(__dirname + '/dist/election-frntnew/index.html'));
-// });
+// app.get('*', function (req, res) {
+//     res.sendFile('index.html');
+//   });
+app.get('/', function(req, res) {
+    console.log('__dirname',path.join(__dirname + '/election-frntnew/index.html'))
+    res.sendFile(path.join(__dirname + '/election-frntnew/index.html'));
+});
 
 app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));

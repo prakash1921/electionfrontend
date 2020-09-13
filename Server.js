1e33397d01645d1919a3b0b526fd16f4bc064d7f
prakash1921 <dhobi.prakash1920@gmail.com>
2020-09-13 18:36:32 +0530

    changes done

--- Server.js
+++ Server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
-// var port = process.env.PORT;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));

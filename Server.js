597d3cff2a11d5894b99738fd195a7fe93880b8b
prakash1921 <dhobi.prakash1920@gmail.com>
2020-09-13 18:51:56 +0530

    changes done

--- Server.js
+++ Server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
+// var port = process.env.PORT;
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname ,'build', '/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/election-frntnew/index.html'));
 });
 
 app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));

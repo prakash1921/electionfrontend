637a1ae0fc434ae92f1b67caa5668453246ea2f8
prakash1921 <dhobi.prakash1920@gmail.com>
2020-09-13 15:52:29 +0530

    changes done

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -16,7 +16,8 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  // baseUrl: 'http://localhost:4200/',
+  baseUrl:'https://election-frntnew.herokuapp.com/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,

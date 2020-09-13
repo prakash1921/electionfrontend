c40af700616f5cb9d703d8a017ed9262e2a4ce3e
prakash1921 <dhobi.prakash1920@gmail.com>
2020-09-13 18:28:12 +0530

    changes done

--- Server.js
+++ Server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+const port = 3000;
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));

5adf4ff9521db840bb20b825dc43e1db4083fe7c
prakash1921 <dhobi.prakash1920@gmail.com>
2020-09-13 18:13:20 +0530

    changes done

--- Server.js
+++ Server.js
@@ -6,3 +6,6 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
